18abecafcbf4fa4f60b889de4d6fde92501cf5ba
bhanupriya08 <cs100priya@gmail.com>
2020-01-29 17:08:10 +0530

    updated Admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+admin.site.site_header = "Blog Database"
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ("author","title","created_date")
+	list_filter = ("created_date",)
+
+admin.site.register(Post,PostAdmin)
 
 
